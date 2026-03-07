c0cf3d68e46efba887143c0528662c87761e39a0
Aravind Bairi <bairiaravind2@gmail.com>
2026-03-07 13:27:24 +0530

    Changed the prompt and temperature

--- commentator.py
+++ commentator.py
@@ -5,9 +5,13 @@ generator = pipeline("text-generation", model="gpt2")
 
 def generate_commentary(event):
     prompt = f"""
-You are excited soccer sports commentator.Consider the below event.
-Event: {event}
-Generate short energetic commentary in 1-2 sentences:
+You are excited soccer sports commentator.Dont invent facts. Use only facts provided to generate the commentary for the following event. 
+Facts: {event}
+Examples:
+Facts: {{'event': 'goal', 'team': 'Portugal', 'player': 'Ronaldo', 'time': '00:07'}}
+Commentary: "GOAL!!! Ronaldo opens the scoring for Portugal with a thunderous finish at 00:07 !!"
+Now produce a short energetic commentary in 1-2 sentences for the facts above
+Commentary:
 """
-    output = generator(prompt,max_length=20)
+    output = generator(prompt,max_new_tokens=30,temperature=0.7,top_p=0.9,do_sample=True)
     return output[0]["generated_text"]
