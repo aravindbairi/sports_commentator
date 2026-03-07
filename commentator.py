cd40bd9a703603ae3c0a641929a3b2bdf17e7da9
Aravind Bairi <bairiaravind2@gmail.com>
2026-03-07 13:42:22 +0530

    Updated the logging

--- commentator.py
+++ commentator.py
@@ -1,17 +1,22 @@
+import transformers
 from click import prompt
 from transformers import pipeline
+
+transformers.logging.set_verbosity_error()
+transformers.utils.logging.disable_progress_bar()
 
 generator = pipeline("text-generation", model="gpt2")
 
 def generate_commentary(event):
     prompt = f"""
-You are excited soccer sports commentator.Dont invent facts. Use only facts provided to generate the commentary for the following event. 
-Facts: {event}
+You are excited soccer sports commentator. Use only facts provided to generate the commentary. Dont invent facts. 
 Examples:
-Facts: {{'event': 'goal', 'team': 'Portugal', 'player': 'Ronaldo', 'time': '00:07'}}
-Commentary: "GOAL!!! Ronaldo opens the scoring for Portugal with a thunderous finish at 00:07 !!"
-Now produce a short energetic commentary in 1-2 sentences for the facts above
+Facts: {{'event': 'goal', 'team': 'Argentina', 'player': 'Messi', 'time': '81:07'}}
+Commentary: "GOAL!!! Messi delivers a fantastic goal for Argentina with a thunderous finish at 81:07 !!"
+
+Now produce a short energetic commentary in 1-2 sentences for the facts below
+Facts: {event}
 Commentary:
 """
-    output = generator(prompt,max_new_tokens=30,temperature=0.7,top_p=0.9,do_sample=True)
+    output = generator(prompt,max_new_tokens=50,temperature=0.7,top_p=0.9,do_sample=True)
     return output[0]["generated_text"]
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from detector import detect_event
 from ingest import read_video
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
 for i, frame in enumerate(read_video("./videos/test.mov")):
     event = detect_event(i)
